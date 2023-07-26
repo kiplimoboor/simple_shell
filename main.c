fd970b0c138a11513ff6940908202014b42dab1f
kiplmoboor <kiplimoarapboor@gmail.com>
2023-07-27 00:12:49 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - imitates the shell
+ *
+ * Return: (0) is success
+ */
+
+int main(void)
+{
+	char *shell = "/bin/sh";
+	char *args[] = {"/bin/sh", NULL};
+	char *env[] = {NULL};
+
+	execve(shell, args, env);
+	exit(EXIT_FAILURE);
+}
